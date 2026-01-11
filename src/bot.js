753dc29e38dab2e2adb26283d75c39b69d08e1d7
NullDev (Shadow) <chris@nulldev.org>
2026-01-11 14:16:22 +0100

    entry

---
+++ src/bot.js
@@ -0,0 +1,75 @@
+import EventEmitter from "node:events";
+import { ClusterClient, getInfo } from "discord-hybrid-sharding";
+import { GatewayIntentBits, Events, ActivityType, Partials } from "discord.js";
+import Log from "./util/log.js";
+import { config } from "../config/config.js";
+import DiscordClient from "./service/client.js";
+import messageCreate from "./events/messageCreate.js";
+import clientReady from "./events/clientReady.js";
+import shardReady from "./events/shardReady.js";
+import messageReactionAdd from "./events/messageReactionAdd.js";
+
+// ========================= //
+// = Copyright (c) NullDev = //
+// ========================= //
+
+EventEmitter.defaultMaxListeners = 20;
+
+const client = new DiscordClient({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildMessageReactions,
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.Message,
+        Partials.Reaction,
+    ],
+    presence: {
+        status: "dnd",
+        activities: [{ name: "Starting...", type: ActivityType.Playing }],
+    },
+    shards: getInfo().SHARD_LIST,
+    shardCount: getInfo().TOTAL_SHARDS,
+});
+
+Log.wait("Starting bot...");
+
+client.cluster = new ClusterClient(client);
+
+client.on(Events.ClientReady, async() => clientReady(client));
+
+client.on(Events.ShardReady, async shard => shardReady(client, shard));
+
+client.on(Events.MessageCreate, message => messageCreate(message));
+
+client.on(Events.MessageReactionAdd, async(reaction, user) => messageReactionAdd(reaction, user));
+
+client.on(Events.GuildCreate, async guild => Log.info("Joined guild: " + guild.name));
+
+client.on(Events.GuildDelete, guild => Log.info("Left guild: " + guild.name));
+
+client.on(Events.GuildUnavailable, guild => Log.warn("Guild is unavailable: " + guild.name));
+
+client.on(Events.Warn, info => Log.warn(info));
+
+client.on(Events.Error, err => Log.error("Client error.", err));
+
+client.on(Events.Debug, info => Log.debug(info, true));
+
+client.on(Events.CacheSweep, info => Log.debug("Cache sweep: " + info));
+
+client.on(Events.ShardError, (error, shardId) => Log.error(`Shard ${shardId} encountered an error:`, error));
+
+client.on(Events.ShardDisconnect, (event, shardId) => Log.warn(`Shard ${shardId} disconnected with code ${event.code} and reason: ${event.reason}`));
+
+client.login(config.discord.bot_token)
+    .then(() => Log.done("Logged in!"))
+    .catch(err => Log.error("Failed to login: ", err));
+
+process.on("unhandledRejection", (
+    /** @type {Error} */ err,
+) => Log.error("Unhandled promise rejection: ", err));
