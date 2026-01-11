8012d25ad16a6bedb213cad25a70e184d71db407
NullDev (Shadow) <chris@nulldev.org>
2026-01-11 14:15:34 +0100

    colnfigs

---
+++ config/config.template.js
@@ -0,0 +1,14 @@
+export default {
+    discord: {
+        bot_token: "",
+        bot_owner_ids: [],
+        wolfram_appid: "",
+        total_shards: /** @type {number | "auto" | undefined} */ (1),
+    },
+    ids: {
+        guild_id: "",
+        daily_int_channel: "",
+        daily_int_role: "",
+        meme_channel: "",
+    },
+};
