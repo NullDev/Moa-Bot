b3eac11afb1eea9ed514555a311502b8404d7ce6
NullDev (Shadow) <chris@nulldev.org>
2026-01-19 14:50:46 +0100

    add dad joke

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -21,37 +21,46 @@ const messageCreate = async function(message){
 
     if (message.partial) return;
 
+    const msg = message.content.trim();
+    const cleanedMsg = msg.toLowerCase().replace(/[^a-z0-9öäüß ]/g, "").trim();
+
     const memeChannelId = config.ids.meme_channel;
     if (memeChannelId && message.channelId === memeChannelId){
         const hasImage = message.attachments.some(attachment => attachment.contentType?.startsWith("image/"));
         if (hasImage){
             const randomNum = Math.random();
             if (randomNum < 0.1){
-                await message.reply("moa");
+                await message.reply("moa").catch();
             }
             else if (randomNum < 0.2){
-                await message.reply("kiwi");
+                await message.reply("kiwi").catch();
             }
         }
     }
 
-    if (message.content.toLowerCase().includes("moa")){
-        try {
-            await message.react("1459951228145500374");
-        }
-        // eslint-disable-next-line no-unused-vars
-        catch (error){
-            // ignore
-        }
+    if (cleanedMsg.includes("moa")){
+        await message.react("1459951228145500374").catch();
     }
 
-    if (message.content.toLowerCase().includes("peak")){
-        try {
-            await message.react("1444063284407046335");
-        }
-        // eslint-disable-next-line no-unused-vars
-        catch (error){
-            // ignore
+    if (cleanedMsg.includes("peak")){
+        await message.react("1444063284407046335").catch();
+    }
+
+    else if (cleanedMsg.startsWith("im") || cleanedMsg.startsWith("i am") || cleanedMsg.startsWith("i'm")){
+        const shouldSend = Math.random() <= 0.5; // 50% chance to respond
+        if (!shouldSend) return;
+
+        const words = cleanedMsg.split(" ").filter(Boolean);
+        if (words.length <= 5){
+            let name = words.slice(2).join(" ");
+            if (name.length > 0){
+                const firstLetter = name.charAt(0).toUpperCase();
+                const rest = name.slice(1);
+                name = firstLetter + rest;
+            }
+            if (name.length > 0 && name.length <= 32){
+                await message.reply(`Hello ${name}, I am Moa Bot. :wave:`).catch(() => {});
+            }
         }
     }
 };
